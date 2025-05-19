5646b0eeb0ac4b0f6fd5d823c3af5990fcb1e4ba
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2025-05-19 17:55:08 +0100

    refactors checking events into seperate functions

--- star_attack.py
+++ star_attack.py
@@ -31,15 +31,21 @@ class StarAttack():
                 if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        self.ship.moving_up = True
-                    elif event.key == pygame.K_DOWN:
-                        self.ship.moving_down = True
+                    self._check_keydown_events(event)
                 elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_UP:
-                        self.ship.moving_up = False
-                    elif event.key == pygame.K_DOWN:
-                        self.ship.moving_down = False
+                    self._check_keyup_events(event)
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True       
 
 
 if __name__ == "__main__":
