0a7eb347af3cb96c8a468560d0eaa88a55e98ee9
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2025-05-19 17:48:17 +0100

    adds functions to check for keydown events and move the ship up and down

--- ship.py
+++ ship.py
@@ -12,11 +12,25 @@ class Ship():
         self.image = pygame.image.load('images/spaceship.bmp')
         self.image = pygame.transform.scale(self.image, (130, 130))
         self.rect = self.image.get_rect()
+        self.speed = 1.5
 
         # Start ship at centre left of the screen.
         self.rect.left = self.screen_rect.left
 
-        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+        # Movement flags
+        self.moving_down = False
+        self.moving_up = False
     
+    def update(self):
+        """Update the spaceship's position based on the movement flags."""
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.speed
+        
+        self.rect.y = self.y
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
--- star_attack.py
+++ star_attack.py
@@ -18,13 +18,24 @@ class StarAttack():
 
     def run_game(self):
         while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
             self.screen.fill(self.bg_colour)
             self.ship.blitme()
             pygame.display.flip()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """Respond to key presses and mouse events."""
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.ship.moving_up = True
+                    if event.key == pygame.K_DOWN:
+                        self.ship.moving_down = True
+
 
 if __name__ == "__main__":
     space_game = StarAttack()
